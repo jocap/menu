b5bffdac77f6a4d0237b8ff0edfee741e2413bad
John Ankarström <john@ankarstrom.se>
2019-01-26 01:31:37 +0100

    read items from stdin

--- menu.c
+++ menu.c
@@ -6,6 +6,7 @@ struct binding {
 };
 
 int main(int argc, char *argv[]) {
+	/* process arguments */
 	struct binding bindings[argc - 1];
 
 	if (argc > 1)
@@ -14,6 +15,29 @@ int main(int argc, char *argv[]) {
 			if (n < 2) goto usage;
 		}
 
+	/* read items */
+	size_t max_count = 100;
+	char **lines;
+	if ((lines = calloc(max_count, sizeof(char *))) == NULL)
+		err(1, "calloc");
+
+	char *line = NULL;
+	size_t size = 0;
+	ssize_t len;
+	size_t count = 0;
+
+	while ((len = getline(&line, &size, stdin)) != -1) {
+		if (count >= max_count) {
+			max_count *= 2;
+			if ((lines = reallocarray(lines, count, sizeof(char *))) == NULL)
+				err(1, "reallocarray");
+		}
+		line[strlen(line) - 1] = '\0'; /* remove trailing newline */
+		lines[count] = strdup(line);
+		count++;
+	}
+	free(line);
+	if (ferror(stdin)) err(1, "getline");
 usage:
 	fprintf(stderr, "usage: %s [key=desc]\n", argv[0]);
 	return 1;
